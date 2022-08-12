20d75b1907e1bf11aa358ec762aa6b7df0e12a87
hei-franklin <hei.franklin.2@gmail.com>
2022-08-12 13:52:30 +0300

    first commit init

---
+++ src/main/java/com/taas/TaasGradeApi/TaasGradeApiApplication.java
@@ -0,0 +1,13 @@
+package com.taas.TaasGradeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaasGradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaasGradeApiApplication.class, args);
+	}
+
+}
